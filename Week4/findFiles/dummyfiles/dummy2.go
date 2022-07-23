c79c842192cafbefdf65778ead34a0a02c6835db
zinedburak <zined_burak@hotmail.com>
2022-07-23 13:13:51 +0300

    week 4 find files

--- Week4/findFiles/dummyfiles/dummy2.go
+++ Week4/findFiles/dummyfiles/dummy2.go
@@ -1,1 +1,1 @@
-package dummyfiles
+package dummyFiles
--- Week4/findFiles/main.go
+++ Week4/findFiles/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
+func main() {
+	err := filepath.Walk("./findFiles/dummyFiles",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if strings.HasSuffix(path, ".go") {
+				fmt.Println(path, info.Size())
+			}
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
 }
